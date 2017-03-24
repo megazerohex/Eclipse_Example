4274de5f495318069924aaac4d31ef3890d4a07c
212602240 <212602240@G8996J02E.logon.ds.ge.com>
2017-03-24 16:27:49 -0400

    ver ultima

--- GitHubTest/src/Example_Sofy.java
+++ GitHubTest/src/Example_Sofy.java
@@ -3,5 +3,8 @@ public class Example_Sofy {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("I know, cliche");
+		
+		System.out.println("Puneta quiero salir"
+				+ "Quiero weekend! T.T");
 	}
 }
