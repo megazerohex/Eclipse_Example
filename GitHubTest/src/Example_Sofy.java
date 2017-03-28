2583ac198ee19bc43355b89b8660cdaf7c655de2
212602261 <212602261@GCQ81YY1E.logon.ds.ge.com>
2017-03-28 16:53:25 -0400

    test

--- GitHubTest/src/Example_Sofy.java
+++ GitHubTest/src/Example_Sofy.java
@@ -3,15 +3,10 @@ public class Example_Sofy {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("I know, cliche");
-
 	
-	System.out.println("Error");
-
-		
 		System.out.println(" quiero salir"
 				+ "Quiero weekend! T.T");
 		
 		System.out.println(" i made a mistake");
-
 	}
 }
