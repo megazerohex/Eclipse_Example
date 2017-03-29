90a01d022129c3fc15ec88f55199f2c4ead9625b
212602261 <212602261@GCQ81YY1E.logon.ds.ge.com>
2017-03-29 15:52:20 -0400

    test2

--- GitHubTest/src/Example_Sofy.java
+++ GitHubTest/src/Example_Sofy.java
@@ -1,5 +1,13 @@
+import java.awt.Container;
 
-public class Example_Sofy {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Example_Sofy extends JFrame {
+	private JPanel contentPane;
+	
+	
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("I know, cliche");
@@ -8,5 +16,22 @@ public class Example_Sofy {
 				+ "Quiero weekend! T.T");
 		
 		System.out.println(" i made a mistake");
+	
+	Example_Sofy frame = new Example_Sofy();
+	frame.setVisible(true);
+	
 	}
-}
+	public Example_Sofy() {
+
+	setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	//this.setResizable(false);
+	setBounds(100, 100, 653, 400);
+	contentPane = new JPanel();
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+	contentPane.setLayout(null);
+	
+	
+	}
+	}
+
